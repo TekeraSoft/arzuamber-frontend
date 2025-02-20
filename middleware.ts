5b15d596a1980b3e574800c7aa34fc4c4b9050b3
CodeWithBayramali <alidrkk07@gmail.com>
2025-02-20 12:11:34 +0300

    solve-middleware

--- middleware.ts
+++ middleware.ts
@@ -5,40 +5,37 @@ import { jwtDecode } from "jwt-decode";
 
 export default async function middleware(req) {
   const intlMiddleware = createMiddleware(routing);
-  const res = await intlMiddleware(req); // i18n middleware çağrısı
 
-  const token = req.cookies.get("session-token")?.value;
+  const res = await intlMiddleware(req); // i18n routing için mevcut middleware çağrısı
+
+  const token =
+    req.cookies.get("session-token")?.value ||
+    req.cookies.get("__Secure-next-auth.session-token");
+  if (token) {
+    req.cookies.set("Authorization", `Bearer ${token}`);
+  }
   const locale = req.cookies.get("NEXT_LOCALE")?.value;
 
-  if (!token) {
-    return res; // Kullanıcı zaten giriş yapmamış
-  }
+  const decodedToken = token ? jwtDecode(token) : undefined;
 
-  let decodedToken;
-  try {
-    decodedToken = jwtDecode(token);
-  } catch (error) {
-    console.error("JWT decode hatası:", error);
-    return res;
-  }
-
+  // Admin route koruma
   const isAdminRoute = req.nextUrl.pathname.startsWith(`/${locale}/admin`);
-  const isTokenExpired = decodedToken?.exp < Math.floor(Date.now() / 1000);
 
-  if (isAdminRoute && (isTokenExpired || !decodedToken?.role.includes("ADMIN"))) {
-
+  if (
+    isAdminRoute &&
+    (!token ||
+      decodedToken?.role[0] !== "ADMIN" ||
+      decodedToken?.exp < new Date().getTime() / 1000)
+  ) {
+    req.cookies.delete("session-token");
+    req.cookies.delete("__Secure-next-auth.session-token");
     const baseUrl = new URL("/", req.url);
-    const response = NextResponse.redirect(baseUrl);
-
-    // ✅ Kullanıcıyı sistemden çıkar ve token'ı temizle
-    response.cookies.set("session-token", "", { expires: new Date(0) });
-
-    return response;
+    return NextResponse.redirect(baseUrl);
   }
 
   return res;
 }
 
 export const config = {
-  matcher: ["/", "/(tr|en)/:path*", "/(tr|en)/admin/:path*"],
+  matcher: ["/", "/(tr|en)/:path*", `/(tr|en)/admin/:path*`],
 };
