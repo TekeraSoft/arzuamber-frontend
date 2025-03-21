dbd3296d55ea8e7120052425b5b4dd00b9fd319a
CodeWithBayramali <alidrkk07@gmail.com>
2025-03-21 11:53:34 +0300

    21-03-25-fc-1

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "192.168.1.136",
+        hostname: "localhost",
       },
     ],
     domains: ["www.facebook.com"],
